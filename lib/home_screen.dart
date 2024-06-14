0e8021f59f2574f850c870aa1c2cb30ab682ac47
Yatharth Dhingra <yatharthdhingra1@gmail.com>
2024-06-15 00:19:15 +0530

    styled first page

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,32 +1,115 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+//stateful because we can change the name
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  bool isEditing = false; //tracks whether user is currently editing or not
+  String playerName = "User1"; //contains the player name
+  late TextEditingController
+      _controller; //It allows you to manipulate the text, listen to changes, and manage the focus of the TextField.
+  //late means a non nullable variable will definitely be initialized later (assurance to compiler)
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: playerName);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const SizedBox(height: 20), // Space between app bar and game title
-            const Text(
-              'ORBITO',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'ARCADE'
-              ),
-            ),
+            const Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(top: 100.0),
+                  child: Text(
+                    'ORBITO',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 231, 45, 123),
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'ARCADE'),
+                  ),
+                )),
             const SizedBox(height: 40), // Space between title and buttons
             // Player name input field
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Enter your name',
-                border: OutlineInputBorder(),
+            Padding(
+              padding: const EdgeInsets.only(left: 80.0, right: 50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    //boolean ? return value 1  : return value 2;
+                    child: isEditing
+                        ? TextField(
+                            controller: _controller,
+                            onSubmitted: (value) {
+                              //when i press enter , it should also update
+                              setState(() {
+                                playerName = value;
+                                isEditing = false;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: const BorderSide(
+                                  width: 1,
+                                  color: Colors.white, // Border color
+                                ),
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                            ),
+                          )
+                        : Text(
+                            playerName,
+                            style: const TextStyle(
+                              fontSize: 28,
+                              fontFamily: 'ARCADE',
+                              fontStyle: FontStyle.italic,
+                              color: Color.fromARGB(255, 139, 238, 25),
+                            ),
+                          ),
+                  ),
+                  const SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (isEditing) {
+                          // When leaving editing mode, update player name with controller's text
+                          playerName = _controller.text;
+                        } else {
+                          // When entering editing mode, set the controller's text to player name
+                          _controller.text = playerName;
+                        }
+                        isEditing = !isEditing; // Toggle editing mode
+                      });
+                    },
+                    child: Icon(
+                      isEditing ? Icons.done : Icons.edit,
+                      color: const Color.fromARGB(255, 139, 238, 25),
+                    ),  
+                  ),
+                ],
               ),
-              // Implement logic to store player name
             ),
             const SizedBox(height: 20),
             // Single Player button
